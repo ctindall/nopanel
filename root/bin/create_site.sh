9523e2779e0a781533f3bb08b04e11ee1cfe00a8
Cameron Tindall <ctindall@gmail.com>
2017-07-09 20:38:43 -0500

    first steps toward LP style

--- root/bin/create_site.sh
+++ root/bin/create_site.sh
@@ -5,16 +5,18 @@ user=$2
 
 docroot="/var/www/$domain"
 conf="/etc/apache2/sites-available/$domain.conf"
-
 echo -e "I'll try to create a site with domain '$domain' and matching user '$user'.\n"
+
 
 echo -n "Creating new user and user-group..."
 useradd -M -U -s /bin/nologin "$user"
 echo "Done!"
 
+
 echo -n "Creating new document in '$docroot'..."
 mkdir -p "$docroot"
 echo "Done!"
+
 
 echo -n "Creating new apache config '$conf'..."
 cp "/root/templates/site-template.conf" "/etc/apache2/sites-available/$domain.conf"
@@ -22,10 +24,12 @@ sed -i "s/{domain}/$domain/g" "$conf"
 sed -i "s/{user}/$user/g" "$conf"
 echo "Done!"
 
+
 echo -n "Creating test page at '$docroot/index.php'..."
 echo "<?php phpinfo(); ?>" > $docroot/index.php
 chown -R $user. $docroot
 echo "Done!"
+
 
 a2ensite $domain
 
