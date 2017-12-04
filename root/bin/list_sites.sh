bd4cc23fae9156a0be2c0abce71b70e83c7f384b
Cameron Tindall <ctindall@gmail.com>
2017-12-04 11:14:17 -0600

    exclude html,www, from list_sites

--- root/bin/list_sites.sh
+++ root/bin/list_sites.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find  /var/www/ -maxdepth 1 -type d -printf '%f\t%u\n' | grep -v ^\\. | grep -v ^html$ | grep -v ^htpasswd$
+find  /var/www/ -mindepth 1 -maxdepth 1 -type d -printf '%f\t%u\n' | grep -v ^\\. | grep -v ^html | grep -v ^htpasswd
